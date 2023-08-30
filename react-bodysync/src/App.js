348a79cb5227a000fd7136aa2927448395eddbdd
Jay Yang <106880500+j1yang@users.noreply.github.com>
2023-08-30 09:06:52 -0400

    avatar changed

--- react-bodysync/src/App.js
+++ react-bodysync/src/App.js
@@ -1,28 +1,34 @@
-import React, { useRef } from 'react';
-import './App.css';
-import Avatar from './components/avatar/Avatar';
+import React, { useRef } from "react";
+import "./App.css";
+import Avatar from "./components/avatar/Avatar";
 import { Canvas } from "@react-three/fiber";
-import AnimationBlob from './components/animationBlob/animationBlob';
+import AnimationBlob from "./components/animationBlob/animationBlob";
 
 function App() {
-  const videoInput = useRef()
-  const preLoad = useRef()
+  const videoInput = useRef();
+  const preLoad = useRef();
   return (
-  <div className="App">
-    <div className="extra-info">
-      <h1>
-        Upper-Body Sync
-      </h1>
-      <h2 className="preload" ref={preLoad}>
-        Reading your pose... 
-      </h2>
-    </div>
+    <div className="App">
+      <div className="extra-info">
+        <h1>
+          Face/Body Sync <br />
+          for Desktop Users
+        </h1>
+        <h2 className="preload" ref={preLoad}>
+          Reading your pose...
+        </h2>
+      </div>
       <Canvas className="canvas">
         {/* <OrbitControls enableZoom={false} enablePan={false} /> */}
         <ambientLight intensity={0.6} />
         <directionalLight position={[-2, 5, 2]} intensity={1} />
-        <AnimationBlob/>
-        <Avatar scale={3} position={[-1, -3, 1]} video={videoInput} preload={preLoad}/>
+        <AnimationBlob />
+        <Avatar
+          scale={3}
+          position={[-1, -3, 1]}
+          video={videoInput}
+          preload={preLoad}
+        />
       </Canvas>
       <video hidden ref={videoInput} width="1920px" height="1080px"></video>
     </div>
